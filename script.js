e064717240fc0fac12161d463a9254e6b45dbda3
mohabbro <elabedymidoxl@gmail.com>
2024-04-30 22:26:08 +0300

    Add Tasks in LocalStorage

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
   ![2] Check if Task is exist
   ![3] Create delete all tasks Button
   ![4] Create Finish all tasks Button
-  ?------------------
-  [5] Add To Tasks To The Local Storage
-  [6] Add Drag drop and drag drop to To The Local Storage
-  [7] add Date on task  
+  TODO:------------------ Hard Tasks ------------------
+  ![5] Add To Tasks To The Local Storage
+  ![6] add Date on task  
+  ?[7] Add Drag drop and drag drop to To The Local Storage
   [8] add all tasks is finished to element Tasks is Finished
   [9] add all tasks is Deleted to element Tasks is Deleted
 */
@@ -26,7 +26,72 @@ var svgCode = '<svg class="delete" width="14" height="16" xmlns="http://www.w3.o
 window.onload = function () {
   theInput.focus();
 }
-// adding the task
+
+function addTask(text, formattedDate) {
+  // Check if the task already exists
+  let taskExists = Array.from(document.querySelectorAll('.task-box')).some(task => {
+    // Extract the task description from task.textContent
+    let taskText = task.textContent.trim();
+    let taskDescription = taskText.split('Time of creation:')[0];
+    // Compare the extracted task description with the input text
+    let comparisonResult = taskDescription.trim() === text;
+    // console.log("Task text before extraction:", task.textContent.trim());
+    // console.log("Task text after extraction:", taskDescription.trim());
+    // console.log("Input text:", text);
+    // console.log("Comparison result:", comparisonResult);
+    return comparisonResult;
+  });
+
+  if(taskExists){
+    // Task already exists, show error message
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Task exists, type a different task",
+    });
+  } else {
+    // Create span element 
+    let mainSpan = document.createElement('span');
+    deleteAll.style.display = 'block';
+    finishAll.style.display = 'block';
+
+    // Append Delete Button  
+    mainSpan.innerHTML = svgCode;
+
+    // Add new text  
+    mainSpan.appendChild(document.createTextNode(text));
+
+    // Add class to mainElement
+    mainSpan.className += 'task-box';
+
+    // Format the date and time as desired
+    // Add Element Date  
+    let elementDate = document.createElement('span');
+    elementDate.setAttribute('class', 'date-ForElement')
+    elementDate.innerHTML = `Time of creation: ${formattedDate}`;
+    // Append element Date to main span
+    mainSpan.appendChild(elementDate);
+
+    // Store task details in local storage
+    let taskDetails = {
+      text: text,
+      dateCreated: formattedDate
+    };
+    // Retrieve existing tasks from local storage or initialize as an empty array
+    let storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    // Add the new task to the array
+    storedTasks.push(taskDetails);
+    // Store the updated tasks array back in local storage
+    localStorage.setItem('tasks', JSON.stringify(storedTasks));
+    
+    // Add the task to the container
+    tasksContainer.appendChild(mainSpan);
+
+    // calculate tasks
+    calculate();
+  }
+}
+
 theAddButton.onclick = function () {
   // if input is empty 
   if (theInput.value === '') {
@@ -35,67 +100,87 @@ theAddButton.onclick = function () {
       title: "Oops...",
       text: "You should write task before to press!",
     });
-    // Task One
-  }else{
-    let noTasksMsg = document.querySelector('.no-tasks-Massage');
-    // check if span with no tasks message is exist
-    if (document.body.contains(document.querySelector('.no-tasks-Massage'))) {
-      // Remove no Tasks Message
-      noTasksMsg.remove()
-    }
+  } else {
+    toggleNoTasksMessage()
     // Add text to span
     let text = theInput.value.trim(); // Trim to remove leading and trailing spaces
-    // Check if the task already exists
-    let taskExists = Array.from(document.querySelectorAll('.task-box')).some(task => {
-      return task.textContent.trim() === text;
-    });
-    if(taskExists){
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Task exists, type a different task",
-      });
-    }else{
-      // create span element 
-      let mainSpan = document.createElement('span');
-      deleteAll.style.display = 'block';
-      finishAll.style.display = 'block';
+    // Create a new Date object to get the current date and time
+    let currentDate = new Date();
+    // Format the date and time as desired
+    let formattedDate = `${currentDate.getFullYear()}/${currentDate.getMonth() + 1}/${currentDate.getDate()} ${currentDate.getHours()}:${currentDate.getMinutes()} ${currentDate.getHours() >= 12 ? 'PM' : 'AM'}`;
+    addTask(text, formattedDate);
 
-      // append Delete Button  
-      mainSpan.innerHTML = svgCode;
-      // add new text  
-      mainSpan.appendChild(document.createTextNode(text));
-
-      // add class to mainElement
-      mainSpan.className += 'task-box';
-      // add the task to the container
-      tasksContainer.appendChild(mainSpan);
-
-      // Delete all Tasks when click btn deleteAll
-      deleteAll.addEventListener('click', function(){
-        let allTasks = document.querySelectorAll('.task-box');
-        allTasks.forEach(task => {
-          task.remove();
-        });
-        updateDeleteAllVisibility()
-      })
-
-
-      // empty the input
-      theInput.value = '';
-      // focus on field
-      theInput.focus();
-      // calculate tasks
-      calculate()
-    }
-  };
+    // empty the input
+    theInput.value = '';
+    // focus on field
+    theInput.focus();
+    calculate()
+  }
 };
+
+// Delete all Tasks when click btn deleteAll
+deleteAll.addEventListener('click', function () {
+  let allTasks = document.querySelectorAll('.task-box');
+  allTasks.forEach(task => {
+    task.remove();
+    localStorage.removeItem('tasks')
+  });
+  updateDeleteAllVisibility()
+  // chack number of tasks inside the container
+  if(tasksContainer.childElementCount == 0){
+    noTasks()
+  }
+});
+
+window.onload = function () {
+  // Focus on input field
+  theInput.focus();
+
+  // Check if local storage is supported by the browser
+  if (typeof(Storage) !== "undefined") {
+    // Retrieve tasks from local storage
+    let storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    // Clear existing tasks in the DOM
+    tasksContainer.innerHTML = '';
+    localStorage.clear()
+    // Loop through the stored tasks and add them to the DOM
+    storedTasks.forEach(taskDetails => {
+      addTask(taskDetails.text, taskDetails.dateCreated);
+    });
+    // Update task counts
+    calculate();
+
+    // Check if there are no tasks and display appropriate message
+    if (storedTasks.length === 0) {
+      noTasks();
+    }
+  } else {
+    console.log("Local storage is not supported in this browser.");
+  }
+};
+
 document.addEventListener('click',function(e) {
   if (e.target.classList.contains('delete')) {
     let taskBox = e.target.closest('.task-box');
     if (taskBox) {
+      // Retrieve the task text
+      let taskText = taskBox.textContent.trim().split('Time of creation:')[0].trim();
+
+      // Retrieve existing tasks from localStorage
+      let storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+      // Find the index of the task to be removed
+      let taskIndex = storedTasks.findIndex(task => task.text === taskText);
+
+      // Remove the task from the storedTasks array and update localStorage
+      if (taskIndex !== -1) {
+        storedTasks.splice(taskIndex, 1);
+        localStorage.setItem('tasks', JSON.stringify(storedTasks));
+      }
+      // Remove the task-box element from the DOM
       taskBox.remove();
     }
+
     // chack number of tasks inside the container
     if(tasksContainer.childElementCount == 0){
       noTasks()
@@ -118,8 +203,6 @@ document.addEventListener('click',function(e) {
   }
   calculate()
 }) 
-
-
 
 // function to create no tasks message
 function noTasks(){
@@ -153,71 +236,14 @@ function updateDeleteAllVisibility() {
     deleteAll.style.display = 'block';
     finishAll.style.display = 'block';
   }
+  
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Function to toggle the display of the 'no-tasks-Massage' element based on the presence of tasks
+function toggleNoTasksMessage() {
+  let noTasksMsg = document.querySelector('.no-tasks-Massage');
+  if (document.body.contains(document.querySelector('.no-tasks-Massage'))) {
+    // If there are no tasks, display the message
+    noTasksMsg.remove()
+  } 
+}
